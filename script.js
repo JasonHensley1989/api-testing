9c0b4696ba87273c450cffb4d4ff958d6a120bea
Jason Hensley <jasonhensley1989@gmail.com>
2023-05-16 13:39:43 -0500

    news api testing

--- script.js
+++ script.js
@@ -1,23 +1,28 @@
-console.log("JS Active");
+// console.log("JS Active");
 
-fetch('https://jsonplaceholder.typicode.com/posts')
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(res => res.json())
+//     .then(data => {
+//         const container = document.createElement('div');
+//         const list = document.createElement('ul');
+
+//         data.forEach(item => {
+//             console.log(item.title);
+//             const itemId = document.createElement('li');
+//             const itemTitle = document.createElement('h4');
+//             itemId.textContent = item.id;
+//             itemTitle.textContent = item.title;
+//             list.appendChild(itemId);
+//             list.appendChild(itemTitle)
+//         })
+//         container.appendChild(list);
+//         document.body.appendChild(container)
+//     })
+
+
+// NEWS API
+const apiKey = 'c432f053df844de99a0e1a43560c6ef6';
+
+fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
     .then(res => res.json())
-    .then(data => {
-        const container = document.createElement('div');
-        const list = document.createElement('ul');
-
-        data.forEach(item => {
-            console.log(item.title);
-            const itemId = document.createElement('li');
-            const itemTitle = document.createElement('h4');
-            itemId.textContent = item.id;
-            itemTitle.textContent = item.title;
-            list.appendChild(itemId);
-            list.appendChild(itemTitle)
-        })
-        container.appendChild(list);
-        document.body.appendChild(container)
-    })
-
-
-
+    .then(data => console.log(data))
